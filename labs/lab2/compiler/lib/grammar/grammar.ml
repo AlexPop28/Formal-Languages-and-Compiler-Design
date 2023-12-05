6a9e1e2f84c6d887056c70adbcbd7369e0dfc2f7
Alex Pop <popalexnicolae@gmail.com>
2023-12-05 11:53:13 +0200

    Add [grammar]

---
+++ labs/lab2/compiler/lib/grammar/grammar.ml
@@ -0,0 +1,23 @@
+open! Core
+
+type t = {
+  non_terminals : string list;
+  terminals : string list;
+  starting_symbol : string;
+  productions : (string list * string list) list;
+}
+[@@deriving sexp]
+
+let create ~non_terminals ~terminals ~starting_symbol ~productions =
+  { non_terminals; terminals; starting_symbol; productions }
+
+let create_from_file ~filename =
+  In_channel.read_all filename |> Sexp.of_string |> t_of_sexp
+
+let is_context_free t =
+  List.for_all t.productions ~f:(fun (lhs, _) -> List.length lhs = 1)
+
+let get_non_terminals t = t.non_terminals
+let get_terminals t = t.terminals
+let get_starting_symbol t = t.starting_symbol
+let get_productions t = t.productions
---
+++ labs/lab2/compiler/lib/grammar/grammar.mli
@@ -0,0 +1,17 @@
+open! Core
+
+type t
+
+val create :
+  non_terminals:string list ->
+  terminals:string list ->
+  starting_symbol:string ->
+  productions:(string list * string list) list ->
+  t
+
+val create_from_file : filename:string -> t
+val is_context_free : t -> bool
+val get_non_terminals : t -> string list
+val get_terminals : t -> string list
+val get_starting_symbol : t -> string
+val get_productions : t -> (string list * string list) list
