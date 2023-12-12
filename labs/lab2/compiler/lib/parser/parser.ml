529ed89708b74d7508b0e56cb500e05a8f0961e3
Alex Pop <popalexnicolae@gmail.com>
2023-12-12 18:19:12 +0200

    Add template for [canonical_collection] and some restructuring

--- labs/lab2/compiler/lib/parser/parser.ml
+++ labs/lab2/compiler/lib/parser/parser.ml
@@ -1,75 +1,8 @@
 open! Core
 
-module Lr0_item = struct
-  type t =
-    { lhp : string
-    ; left_dot : string list (* list is backwards *)
-    ; right_dot : string list
-    }
-  [@@deriving compare, hash, sexp, equal]
-
-  let shift t =
-    match t.right_dot with
-    | [] -> Or_error.error_string "Error tried to shift empty righthandside"
-    | right_first :: rest ->
-      Ok { lhp = t.lhp; left_dot = right_first :: t.left_dot; right_dot = rest }
-  ;;
-
-  let create_from_production lhp rhp = { lhp; left_dot = []; right_dot = rhp }
-
-  let to_string_hum t =
-    let left_dot = List.rev t.left_dot |> String.concat ~sep:" " in
-    let right_dot = String.concat t.right_dot ~sep:" " in
-    [%string "[%{t.lhp} -> %{left_dot}.%{right_dot}]"]
-  ;;
-end
-
-module State = struct
-  type t = { items : Lr0_item.t Hash_set.t }
-
-  module Action = struct
-    type t =
-      | Shift
-      | Reduce of (string * string list)
-      | Accept
-    [@@deriving sexp]
-  end
-
-  let get_all_lr0_items_right_dot_starting_with_symbol t symbol =
-    Hash_set.filter t.items ~f:(fun item ->
-      match item.right_dot with
-      | hd :: _ when String.equal hd symbol -> true
-      | _ -> false)
-  ;;
-
-  let equal t1 t2 = Hash_set.equal t1.items t2.items
-  let sexp_of_t t = [%sexp (Hash_set.to_list t.items : Lr0_item.t list)]
-
-  let to_string_hum t =
-    Hash_set.to_list t.items
-    |> List.map ~f:Lr0_item.to_string_hum
-    |> String.concat ~sep:" ; "
-  ;;
-
-  let get_action t (grammar : Enhanced_grammar.t) =
-    let action_list = ref [] in
-    if Hash_set.find t.items ~f:(fun lr0_item -> not (List.is_empty lr0_item.right_dot))
-       |> Option.is_some
-    then action_list := [ Action.Shift ] @ !action_list;
-    Hash_set.iter t.items ~f:(fun lr0_item ->
-      if List.is_empty lr0_item.right_dot
-      then
-        action_list := [ Action.Reduce (lr0_item.lhp, lr0_item.left_dot) ] @ !action_list);
-    let start = grammar.starting_symbol in
-    if Hash_set.find t.items ~f:(fun lr0_item ->
-         String.( = ) start lr0_item.lhp && List.is_empty lr0_item.right_dot)
-       |> Option.is_some
-    then action_list := [ Accept ];
-    match !action_list with
-    | [ action ] -> Ok action
-    | action_list -> Error action_list
-  ;;
-end
+(* TODO open Import *)
+module State = State
+module Lr0_item = Lr0_item
 
 type t = { grammar : Enhanced_grammar.t }
 
--- labs/lab2/compiler/lib/parser/parser.mli
+++ labs/lab2/compiler/lib/parser/parser.mli
@@ -1,31 +1,10 @@
 open! Core
 
+(* open Import *)
+module Lr0_item = Lr0_item
+module State = State
+
 type t
-
-module Lr0_item : sig
-  type t =
-    { lhp : string
-    ; left_dot : string list (* list is backwards *)
-    ; right_dot : string list
-    }
-  [@@deriving compare, hash, sexp, equal]
-end
-
-module State : sig
-  type t = { items : Lr0_item.t Hash_set.t }
-
-  module Action : sig
-    type t =
-      | Shift
-      | Reduce of (string * string list)
-      | Accept
-    [@@deriving sexp]
-  end
-
-  val sexp_of_t : t -> Sexp.t
-  val to_string_hum : t -> string
-  val get_action : t -> Enhanced_grammar.t -> (Action.t, Action.t list) result
-end
 
 val create : Enhanced_grammar.t -> t
 val closure : t -> Lr0_item.t Hash_set.t -> State.t
